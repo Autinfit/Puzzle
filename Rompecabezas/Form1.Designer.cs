e3b3c724e6091e8f0edf9de717904f7c427e723b
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-08-12 23:54:00 -0400

    Editando interfaz...

--- Rompecabezas/Form1.Designer.cs
+++ Rompecabezas/Form1.Designer.cs
@@ -28,7 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 403);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Lista Uno";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(684, 403);
+            label2.Name = "label2";
+            label2.Size = new Size(104, 38);
+            label2.TabIndex = 1;
+            label2.Text = "Estado";
             // 
             // Form1
             // 
@@ -36,11 +60,17 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Gray;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Puzzle";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
     }
 }
